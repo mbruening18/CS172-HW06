00d9b430b022fdde68c865624f4f55bd0198d333
Megan Bruening <mbruening18@my.whitworth.edu>
2016-11-04 14:23:10 -0700

    Start

---
+++ CS172-HW06/CS172-HW06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS172-HW06
+//
+//  Created by Megan Bruening on 11/4/16.
+//  Copyright © 2016 Megan Bruening. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
